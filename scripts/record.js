aad1f7741caf209509fdc5bcd91a7c0fe062ef27
evita <evita.dsilva03@gmail.com>
2025-02-08 03:07:22 +0530

    Generate the record container using js

--- scripts/record.js
+++ scripts/record.js
@@ -1,5 +1,22 @@
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
+let recordHTML = "";
+
+recordHTML = `
+  <div class="hours">
+    <img src="images/capy2.png"/>
+    <div class="time-container">
+      <div>last</div>
+      <div class="time js-time"> 0 </div>
+      <div>mins.</div>
+    </div>
+  </div>
+  <button class="js-record-button">Record</button>
+`;
+
+document.querySelector(".js-record-container")
+  .innerHTML = recordHTML;
+
 const timerDisplay = document.querySelector('.js-time');
 const recordButton = document.querySelector(".js-record-button");
 
