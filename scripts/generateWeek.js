5ea6a1b60906b6eccfa544a110470aff5d88f3ee
evita <evita.dsilva03@gmail.com>
2025-02-07 22:13:30 +0530

    Generate dates using javascript

---
+++ scripts/generateWeek.js
@@ -0,0 +1,29 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
+function generateWeek() {
+  let today = dayjs();
+  let dayIndex = today.day();
+  let startOfWeek = today.subtract(dayIndex, "day");
+
+  let weekHTML ="";
+
+  for(let i=0; i<7; i++){
+    const date = startOfWeek.add(i, "day");
+    const isToday = date.isSame(today, "day");
+
+    weekHTML += `
+      <div class="date-container ${isToday ? "today" : ""}">
+        <div class="day">${date.format('ddd')}</div>
+        <div class="date">${date.format('D')}</div>
+        <div class="mark">
+          <img src="images/capy3.png"/>
+        </div>
+      </div>
+    `;
+  }
+
+  document.querySelector('.js-week').innerHTML = weekHTML;
+
+}
+
+generateWeek();
